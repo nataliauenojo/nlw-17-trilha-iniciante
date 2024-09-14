c522a44d2280d78a3356ea5d52a1a65b6d10c9c5
nataliauenojo <nataliauenojo@gmail.com>
2024-09-14 00:06:17 -0300

    fundamentos 002

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-// arrays ,object
  let meta = {
     value: "ler um livro todo mês" ,
-    checked: false,
-    log: (info) => {
-        console.log(info)
+    checked: false
     }
- }
+let meta = [
+    meta,
+    {
+        value: "caminhar 20 minutos todo os dia" ,
+        checked: false
+    }
+]
 
- meta.value = "Não é mais ler um livro"
- meta.log(meta.value);
-
- // function //allow fuction
- //const criarMeta = () => {}
-
- //function criarMeta = () => {}
+console.log(meta[0].value)
