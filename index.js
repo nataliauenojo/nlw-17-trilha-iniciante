ff845ae6f140d16519bc348313a218ee7259772f
nataliauenojo <nataliauenojo@gmail.com>
2024-09-15 15:21:42 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -60,10 +60,28 @@ const metasRealizadas = async () =>{
     }
 
     await select({
-        message: " Metas Realizadas",
+        message: " Metas Realizadas"  + realizadas.length,
         choices: [...realizadas]
     })
 
+
+
+}
+
+const metasAbertas = async () =>{
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true; //return !meta.checked;
+    })
+
+    if(abertas.length == 0){
+        console.log('Não existe metas abertas! :)')
+        return;
+    }
+
+    await select({
+        message: "Metas Abertas"  + abertas.length,
+        choices:[...abertas]
+    })
 
 
 }
@@ -87,6 +105,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -103,6 +125,9 @@ const start = async () => {
                 break;
             case "realizadas":
                 await metasRealizadas();
+                break;
+            case "abertas":
+                await metasAbertas();
                 break;
             case "sair":
                 console.log('Até a proxima!');
